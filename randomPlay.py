c55006f7142fa78863446bb8b0a6ab1bc44ebcad
Daniel Ellen (dhe) <dheandco@gmail.com>
2014-06-29 04:20:16 -0700

    Update randomPlay.py
    minor edits and examples for shrink

--- randomPlay.py
+++ randomPlay.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-#!/usr/bin/env python
-
 # Some of these functions are incomplete I guess,
 # going off of the notes for the last function.
 # I will come back soon and work on this, or at least decide not to.
@@ -15,21 +13,28 @@ import math
 quotient, and it then divides the quotient by the same divisor, and it
 then it divides that new quotient, and it repeats this process for as long as
 the operation continues to produce an integer quotient or once the quotient
-equals 1. '''
-def shrink(z,y):
+equals 1. So...
+
+shrink(1999, 2)  =>  125
+shrink(1000, 3)  =>  1000
+shrink(1000, 4)  =>  250
+shrink(1000, 10)  =>  1
+'''
+def shrink(z, y):
     while float(z)/y == int(z/y): z /= y
     return z
 
 ''' This function tests for whether the list passed to it only contains primes
 after the first element. Should not be used on its own unless you only want to
-know if alist[1:] are all primes.'''
+know if alist[1:] are all primes. In other words, I wrote for use in other
+functions.'''
 def only_dividend_and_primes(alist):
-    return True if False not in map(test_for_prime,alist[1:]) else False
+    return True if False not in map(test_for_prime, alist[1:]) else False
 
 def test_for_prime(z):
     if z == 1:
         return False
-    for i in xrange(2,int(math.sqrt(z)+1)):
+    for i in xrange(2, int(math.sqrt(z)+1)):
         if z % i == 0:
             return False
     return True
